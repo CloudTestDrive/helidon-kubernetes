499fa81e1160a4bbe40828ff42b1ff82c0dfe3a1
tim_graves <28924492+atimgraves@users.noreply.github.com>
2020-05-15 12:37:21 +0100

    added linkerd-ingress.yaml file for controlling access to the linkerd deployment

--- base-kubernetes/runStack.sh
+++ base-kubernetes/runStack.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo creating tls secret
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=nginxsvc/O=nginxsvc"
+kubectl create secret tls tls-secret --key tls.key --cert tls.crt
 ./setupClusterIPServices.sh
 ./setupIngress.sh
 ./create-secrets.sh
