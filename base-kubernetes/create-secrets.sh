ed08be78ea6d8e0f502e9223fd29884cbf3fd390
tim_graves <28924492+atimgraves@users.noreply.github.com>
2020-11-18 20:21:29 +0000

    fixed typo in create-secrets and added auth forwarding fo stockmanager-confsecure

--- base-kubernetes/create-secrets.sh
+++ base-kubernetes/create-secrets.sh
@@ -14,7 +14,8 @@ echo stockmanagerdb
 kubectl delete secret stockmanagerdb --ignore-not-found=true
 echo Deleted secrets
 echo Secrets remaining in namespace are 
-kubectl get secretsecho Creating stock manager secrets
+kubectl get secret
+secho Creating stock manager secrets
 echo stockmanagerdb
 kubectl apply -f $MGRDIR/databaseConnectionSecret.yaml
 echo sm-wallet-atp
