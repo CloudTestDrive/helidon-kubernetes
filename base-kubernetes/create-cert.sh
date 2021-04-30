b26ce6348837c1b7bef3907fa68ff8a4cfc1d1ec
tim_graves <28924492+atimgraves@users.noreply.github.com>
2021-04-30 16:01:59 +0100

    indentation fixes on commends for yaml files, added a create-cert script to speed up the initial setup

---
+++ base-kubernetes/create-cert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo creating tls secret
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=nginxsvc/O=nginxsvc"
+kubectl create secret tls tls-secret --key tls.key --cert tls.crt -n ingress-nginx
