5e06d7d7c4be4d91c78ed1ec2f51eb03a91b1808
tim_graves <28924492+atimgraves@users.noreply.github.com>
2021-05-05 15:42:11 +0100

    prompt for ingress controller ready

--- base-kubernetes/runStack.sh
+++ base-kubernetes/runStack.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo creating tls secret
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=nginxsvc/O=nginxsvc"
-kubectl create secret tls tls-secret --key tls.key --cert tls.crt
+echo You must have setup the tls secret and ingress controller before running this script
+read -p "Ready to procees with namespace $1 ? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo OK, existing, if needed please use the create-cert.sh script to setup the tls secret and install the ingress controller using Helm
+    exit 1
+fi
 ./setupClusterIPServices.sh
 ./setupIngress.sh
 ./create-secrets.sh
