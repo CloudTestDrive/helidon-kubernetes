400d7dfa60e008389d478ebd5b47fb011205e6cd
tim_graves <28924492+atimgraves@users.noreply.github.com>
2021-06-30 20:12:24 +0100

    update the runStack script as we're using the different setup.

--- base-kubernetes/runStack.sh
+++ base-kubernetes/runStack.sh
@@ -13,6 +13,13 @@ then
     echo "OK, please create the ingress service and get the external IP address"
     exit 1
 fi
+read -p "Have you created the root CA and tls certificate files (tls-store.crt and tls-store.key) using the external IP address in the common name? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo OK, existing, if needed please use the create-cert.sh script to setup the tls secret
+    exit 1
+fi
 read -p "Have you edited the ingressConfig.yaml to replace the external IP address ? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -20,14 +27,7 @@ then
     echo 'OK, edit ingressConfig.yaml replacing <External IP> with the IP address of the ingress service (there are multple entries excluding the comments)'
     exit 1
 fi
-read -p "Have you created the tls certificate files (tls-store.crt and tls-store.key) using the external IP address in the common name? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo OK, existing, if needed please use the create-cert.sh script to setup the tls secret
-    exit 1
-fi
-echo Please check the output to make sure that the right context is selected as the default below ?
+echo Please check the output to make sure that the right context is selected as the default below
 kubectl config get-contexts
 read -p "Is the right cluster selected ? " -n 1 -r
 echo    # (optional) move to a new line
