a6990bd371b4f90c6a2bb7be2629c3006cba6c45
tim_graves <28924492+atimgraves@users.noreply.github.com>
2021-08-20 17:03:07 +0100

    typo fixes

--- setup/configure-git-and-run-oke-stack.sh
+++ setup/configure-git-and-run-oke-stack.sh
@@ -3,7 +3,7 @@ if [ $# -lt 2 ]
     echo "Missing arguments, you must provide the name of the namespace to use and the External IP address of the ingress controller service in that order"
     exit -1
 fi
-if ([ $# -eq 2]
+if [ $# -eq 2 ]
    then
      echo About to configure using a department name of $1, an namespace of $1 and a ingress eternal IP of $2
      read -p "Proceed ?  " -n 1 -r
@@ -16,6 +16,6 @@ if ([ $# -eq 2]
 fi
 cd kubernetes-labs
 echo Configuring git repo with department of $1
-bash ./configure-downloaded-git-repo.sh $1
+bash ./configure-downloaded-git-repo.sh $1 skip
 echo Configuring Kubernetes namepace $1 using ingress external IP of $2
 bash ./executeRunStack.sh $1 $2
