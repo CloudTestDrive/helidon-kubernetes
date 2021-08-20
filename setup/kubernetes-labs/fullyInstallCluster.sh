be75a411f0b3f527e3513fb964c1b31c06eb3634
tim_graves <28924492+atimgraves@users.noreply.github.com>
2021-08-20 18:26:56 +0100

    added full setup options

---
+++ setup/kubernetes-labs/fullyInstallCluster.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, you must provide the name of your department - in lower case and only a-z, e.g. tims"
+    exit -1 
+fi
+if [ $# -eq 1 ]
+  then
+    echo setting up config in downloaded git repo using $1 as the department name and $HOME/Wallet.zip as the DB wallet file.
+    read -p "Proceed ? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+      then
+        echo OK, exiting
+        exit 1
+    fi
+  else
+    echo "Skipping fully install cluster confirmation"
+fi
+
+bash $HOME/helidon-kubernetes/setup/kubernetes-labs/installBaseElements.sh 
+
+source $HOME/clusterSettings
+
+bash $HOME/helidon-kubernetes/setup/kubernetes-labs/executeRunStack.sh $1
