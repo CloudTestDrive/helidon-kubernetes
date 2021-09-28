a5214391b08ded8d6fc937b00f0c474750b70802
tim_graves <28924492+atimgraves@users.noreply.github.com>
2021-09-28 18:21:25 +0100

    update to use the IP address for the certs, to delete all old certs when tearing down stack, and to delete all known namespaces we create. this combined with removing all the processes and sidecar containers) should do the job

--- setup/kubernetes-labs/teardownStack.sh
+++ setup/kubernetes-labs/teardownStack.sh
@@ -13,9 +13,18 @@ if [ $# -eq 0 ]
   else
     echo "Skipping confirmation"
 fi
-echo deleting existing deplpoyments in $NAMESPACE
+echo deleting linkerd-viz namespace - if present
+kubectl delete namespace linkerd-viz --ignore-not-found=true
+echo deleting linkerd namespace - if present
+kubectl delete namespace linkerd --ignore-not-found=true
+echo deleting monitoring namespace - if present
+kubectl delete namespace monitoring --ignore-not-found=true
+echo deleting logging namespace - if present
+kubectl delete namespace logging --ignore-not-found=true
+echo deleting your existing deployments in $NAMESPACE
 kubectl delete namespace $NAMESPACE  --ignore-not-found=true
 echo Blanking saved namespace
 echo NAMESPACE= >> $HOME/clusterSettings
 echo resetting ingress rules file
 bash $HOME/helidon-kubernetes/setup/kubernetes-labs/reset-ingress-config.sh  $ip skip
+bash delete-certs.sh skip
