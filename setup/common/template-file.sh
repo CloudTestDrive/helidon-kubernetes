e8cb8b455f1e149e4aa8518584c4864f8cb55611
tim_graves <28924492+atimgraves@users.noreply.github.com>
2021-11-21 20:28:47 +0000

    var name tidyup

--- setup/common/template-file.sh
+++ setup/common/template-file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -f
-updatefile=$1
+sourcefile=$1
 destfile=$2
 oldtext=$3
 newtext=$4
-echo Templating $updatefile replacing $oldtext with $newtext to destination $destfile
+echo Templating $sourcefile replacing $oldtext with $newtext to destination $destfile
 #echo command is "s/$oldtext/$newtext/"
-cat $updatefile | sed -e "s/$oldtext/$newtext/" > $destfile
+cat $sourcefile | sed -e "s/$oldtext/$newtext/" > $destfile
