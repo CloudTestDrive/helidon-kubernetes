2b375426280bc77e1316c7b7a8a779fed688b324
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-01-17 21:19:00 +0000

    get the args order right

--- setup/common/update-file.sh
+++ setup/common/update-file.sh
@@ -3,10 +3,10 @@ updatefile=$1
 oldtext=$2
 newtext=$3
 if [ $# -gt 3 ]
-then 
-  SED_SEP=/
-else
+then
   SED_SEP=$4
+else
+  SED_SEP=/
 fi
 echo Updating $updatefile replacing $oldtext with $newtext 
 temp="$updatefile".tmp
