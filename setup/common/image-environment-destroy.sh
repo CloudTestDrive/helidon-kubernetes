bebd18c5e10114b19161c0ec1863e9e8d5c7fd59
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-01-18 14:48:23 +0000

    the image commands better checks for home region operation

--- setup/common/image-environment-destroy.sh
+++ setup/common/image-environment-destroy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -f
-
+echo "Getting region environment details"
+OCI_HOME_REGION_KEY=`oci iam tenancy get --tenancy-id $OCI_TENANCY | jq -j '.data."home-region-key"'`
+OCI_HOME_REGION=`oci iam region list | jq -e  ".data[]| select (.key == \"$OCI_HOME_REGION_KEY\")" | jq -j '.name'`
 echo "This script will run the required commands to destroy the container images setup for the lab"
 echo "It will only destroy repositories and tokens created by these scripts, if you reused an existing resource"
 echo "then those resources will not be destroyed, and neither will the compartment containing them"
@@ -14,9 +16,29 @@ echo "If you are not you will need to exit at the prompt and follow the lab inst
 read -p "Are you running in a free trial environment (y/n) ? " REPLY
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-  OCI_HOME_REGION_KEY=`oci iam tenancy get --tenancy-id $OCI_TENANCY | jq -j '.data."home-region-key"'`
-  OCI_HOME_REGION=`oci iam region list | jq -e  ".data[]| select (.key == \"$OCI_HOME_REGION_KEY\")" | jq -j '.name'`
-  echo "OK, you are not in a free trial you will need to do the following in the $HOME/helidon-kubernetes/setup/common directory "
+  if [ $OCI_REGION = $OCI_HOME_REGION ]
+  then
+    echo "You are in your home region and this script will continue"
+    SAFE_TO_DESTROY=true
+  else
+    echo You need to run this script in your home region of $OCI_HOME_REGION, you are running it in $OCI_REGION
+    echo Please switch to your OCI home region and re-run this script
+    SAFE_TO_DESTROY=false
+  fi
+else
+  SAFE_TO_DESTROY=true
+  echo "Thank you for confirming you are in a free trial"
+fi
+
+if [ $SAFE_TO_DESTROY = true ]
+then
+  echo "Let's clean your container image environment up"
+  bash container-image-destroy.sh
+  bash ocir-destroy.sh
+  bash auth-token-destroy.sh
+else
+  echo "OK, you are not in a free trial or your home region, you will need to do the following in"
+  echo "the $HOME/helidon-kubernetes/setup/common directory "
   echo "Run the following in the region you ran the labs in"
   echo "Please run them in this order"
   echo "bash container-image-destroy.sh"
@@ -25,10 +47,4 @@ then
   echo "to destroy the auth token using the script, this script will stop if it's not in your home region"
   echo "bash auth-token-destroy.sh"
   exit 1
-else
-  echo "Thank you for confirming you are in a free trial, let's clean your container image environment up"
 fi
-
-bash container-image-destroy.sh
-bash ocir-destroy.sh
-bash auth-token-destroy.sh
--- setup/common/image-environment-setup.sh
+++ setup/common/image-environment-setup.sh
@@ -1,13 +1,34 @@
 #!/bin/bash -f
+echo "Getting region environment details"
+OCI_HOME_REGION_KEY=`oci iam tenancy get --tenancy-id $OCI_TENANCY | jq -j '.data."home-region-key"'`
+OCI_HOME_REGION=`oci iam region list | jq -e  ".data[]| select (.key == \"$OCI_HOME_REGION_KEY\")" | jq -j '.name'`
 echo "This script will run the required commands to setup your own images"
 echo "It assumes you are working in a free trial environment"
 echo "If you are not you will need to exit at the prompt and follow the lab instructions for setting up the configuration separatly"
 read -p "Are you running in a free trial environment (y/n) ? " REPLY
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-  OCI_HOME_REGION_KEY=`oci iam tenancy get --tenancy-id $OCI_TENANCY | jq -j '.data."home-region-key"'`
-  OCI_HOME_REGION=`oci iam region list | jq -e  ".data[]| select (.key == \"$OCI_HOME_REGION_KEY\")" | jq -j '.name'`
-  echo "OK, you are not in a free trial you will need to do the following in the $HOME/helidon-kubernetes/setup/common directory "
+  if [ $OCI_REGION = $OCI_HOME_REGION ]
+  then
+    echo "You are in your home region and this script will continue"
+    SAFE_TO_BUILD=true
+  else
+    echo You need to run this script in your home region of $OCI_HOME_REGION, you are running it in $OCI_REGION
+    echo Please switch to your OCI home region and re-run this script
+    SAFE_TO_BUILD=false
+  fi
+else
+  SAFE_TO_BUILD=true
+  echo "Thank you for confirming you are in a free trial, let's set your container image environment up"
+fi
+
+if [ $SAFE_TO_BUILD = true ]
+then
+  bash auth-token-setup.sh
+  bash ocir-setup.sh
+  bash container-image-setup.sh
+else
+  echo "OK, you will need to do the following in the $HOME/helidon-kubernetes/setup/common directory "
   echo "In your HOME REGION of $OCI_HOME_REGION (you are currently in $OCI_REGION) you will need"
   echo "to setup the auth token using the script, this script will stop if it's not in your home region"
   echo "bash auth-token-setup.sh"
@@ -16,10 +37,4 @@ then
   echo "bash ocir-setup.sh"
   echo "bash container-image-setup.sh"
   exit 1
-else
-  echo "Thank you for confirming you are in a free trial, let's set your container image environment up"
 fi
-
-bash auth-token-setup.sh
-bash ocir-setup.sh
-bash container-image-setup.sh
