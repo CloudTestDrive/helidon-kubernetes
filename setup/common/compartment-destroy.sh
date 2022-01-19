be46babda32029e259b59755da3622a9da73f5d3
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-01-19 10:24:15 +0000

    change the PARENT_COMPARTMENT_OCID to COMPARTMENT_PARENT_OCID to avoid deletion when removing COMPARTMENT_OCID

--- setup/common/compartment-destroy.sh
+++ setup/common/compartment-destroy.sh
@@ -31,18 +31,18 @@ fi
 
 TENANCY_NAME=`oci iam tenancy get --tenancy-id=$OCI_TENANCY | jq -j '.data.name'`
 COMPARTMENT_NAME=`oci iam compartment get  --compartment-id $COMPARTMENT_OCID | jq -j '.data.name' | sed -e 's/"//g'`
-PARENT_COMPARTMENT_OCID=`oci iam compartment get  --compartment-id $COMPARTMENT_OCID | jq -j '.data."compartment-id"' | sed -e 's/"//g'`
-PARENT_COMPARTMENT_NAME=`oci iam compartment get  --compartment-id $PARENT_COMPARTMENT_OCID | jq -j '.data.name' | sed -e 's/"//g'`
+COMPARTMENT_PARENT_OCID=`oci iam compartment get  --compartment-id $COMPARTMENT_OCID | jq -j '.data."compartment-id"' | sed -e 's/"//g'`
+COMPARTMENT_PARENT_NAME=`oci iam compartment get  --compartment-id $COMPARTMENT_PARENT_OCID | jq -j '.data.name' | sed -e 's/"//g'`
 
-if [ $PARENT_COMPARTMENT_NAME = $TENANCY_NAME ]
+if [ $COMPARTMENT_PARENT_NAME = $TENANCY_NAME ]
 then
   PARENT_NAME="Tenancy root"
 else 
-  PARENT_NAME="$PARENT_COMPARTMENT_NAME sub compartment"
+  PARENT_NAME="$COMPARTMENT_PARENT_NAME sub compartment"
 fi
 
 
-echo Destroying compartment $COMPARTMENT_NAME in $PARENT_COMPARTMENT_NAME This will fail if the compartment is not empty and you will then need to remove any respurce in it and delete it manually
+echo Destroying compartment $COMPARTMENT_NAME in $COMPARTMENT_PARENT_NAME This will fail if the compartment is not empty and you will then need to remove any respurce in it and delete it manually
 oci iam compartment delete  --compartment-id $COMPARTMENT_OCID
 
 
