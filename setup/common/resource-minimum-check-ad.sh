8c4e3dc1c2b038bc8eac5127e7fc37a876bd9e6f
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-02-09 18:22:47 +0000

    added scripts to do a pre-check of the resources

---
+++ setup/common/resource-minimum-check-ad.sh
@@ -0,0 +1,45 @@
+#!/bin/hash -f
+
+if [ $# -lt 4 ]
+then
+  echo 'Missing arguments, this script requires arguments in the following order'
+  echo 'the OCID of the compartment (or tenancy) to do a resource check on'
+  echo 'The service name (e.g. compute)'
+  echo 'The limit name e.g. standard-e4-core-count'
+  echo 'The minimum required number of resources e.g. 3'
+  echo 'It will return 0 if the available number of resources is equal to or more than than the minimum required'
+  echo 'It will return 50 if the available number of resources is less than the minimum required'
+  echo 'It will return 99 if the resource availability cannot be retrieved'
+  echo 'With the arguments above this script will return a zero code if the available number of compute e4 cores equals or exceeds 3'
+  exit 1
+fi
+
+OCID=$1
+SERVICE_NAME=$2
+LIMIT_NAME=$3
+MINIMUM_REQUIRED=$4
+
+AD_NAME=`oci iam availability-domain  list --all --compartment-id $OCID | jq -j '.data[0].name'`
+
+if [ -z "$AD_NAME" ]
+then
+  echo "Unable to get availability domain details, cannot continue"
+  exit 99
+fi
+
+AVAIL_RESOURCES=`oci limits resource-availability get --compartment-id $OCI_TENANCY --availability-domain $AD_NAME --service-name $SERVICE_NAME --limit-name $LIMIT_NAME | jq -j '.data.available'`
+
+if [ -z "$AVAIL_RESOURCES" ]
+then
+  echo "Unable to get resource availability for resource $LIMIT_NAME in service $SERVICE_NAME in availability domain $AD_NAME, cannot continue"
+  exit 99
+fi
+
+if [ $AVAIL_RESOURCES -lt $MINIMUM_REQUIRED ]
+then 
+  echo "You asked for $MINIMUM_REQUIRED of resource $LIMIT_NAME in service $SERVICE_NAME in availability domain $AD_NAME, unfortunately only $AVAIL_RESOURCES are available"
+  exit 50
+else
+  echo "You asked for $MINIMUM_REQUIRED of resource $LIMIT_NAME in service $SERVICE_NAME in availability domain $AD_NAME, congratulations $AVAIL_RESOURCES are available"
+  exit 0
+fi
---
+++ setup/common/resource-minimum-check-region.sh
@@ -0,0 +1,35 @@
+#!/bin/hash -f
+
+if [ $# -lt 3 ]
+then
+  echo 'Missing arguments, this script requires arguments in the following order'
+  echo 'The service name (e.g. vcn)'
+  echo 'The limit name e.g. vcn-count'
+  echo 'The minimum required number of resources e.g. 1'
+  echo 'It will return 0 if the available number of resources is equal to or more than than the minimum required'
+  echo 'It will return 50 if the available number of resources is less than the minimum required'
+  echo 'It will return 99 if the resource availability cannot be retrieved'
+  echo 'With the arguments above this script will return a zero code if the available number of vcns equals or exceeds 1'
+  exit 1
+fi
+
+SERVICE_NAME=$1
+LIMIT_NAME=$2
+MINIMUM_REQUIRED=$3
+
+AVAIL_RESOURCES=`oci limits resource-availability get --compartment-id $OCI_TENANCY --service-name "$SERVICE_NAME" --limit-name $LIMIT_NAME | jq -j '.data.available'`
+
+if [ -z "$AVAIL_RESOURCES" ]
+then
+  echo "Unable to get resource availability for resource $LIMIT_NAME in service $SERVICE_NAME in region, cannot continue"
+  exit 99
+fi
+
+if [ $AVAIL_RESOURCES -lt $MINIMUM_REQUIRED ]
+then 
+  echo "You asked for $MINIMUM_REQUIRED of resource $LIMIT_NAME in service $SERVICE_NAME in region, unfortunately only $AVAIL_RESOURCES are available"
+  exit 50
+else
+  echo "You asked for $MINIMUM_REQUIRED of resource $LIMIT_NAME in service $SERVICE_NAME in region congratulations $AVAIL_RESOURCES are available"
+  exit 0
+fi
