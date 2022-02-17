c7237cd89047c2e5c165555ee944e7818bc83a52
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-02-17 11:09:24 +0000

    name changes on  prefix

--- setup/common/oke-module.tf
+++ setup/common/oke-module.tf
@@ -7,7 +7,7 @@ home_region="OCI_HOME_REGION"
 region="OCI_REGION"
 cluster_name="CLUSTER_NAME"
 
-vcn_name="oke-vcn-CLUSTER_NAME"
+vcn_name="vcn-CLUSTER_NAME"
 
 create_bastion_host=false
 create_bastion_service=false
@@ -15,7 +15,7 @@ create_operator=false
 control_plane_type="public"
 control_plane_allowed_cidrs=["0.0.0.0/0"]
 
-label_prefix="K8S_CONTEXT-oke"
+label_prefix="lab-K8S_CONTEXT"
 
 node_pools = {
   pool1 = { shape = "WORKER_SHAPE", ocpus = 1, memory = 16, node_pool_size = 3, boot_volume_size = 50, label = {pool = "pool1" } }
