8083113682f70dabe2522ec1e0b5c5c3dac078ae
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-02-21 18:03:45 +0000

    reformat TEXT_TO_DELETE from testtodelete

--- setup/common/delete-from-saved-settings.sh
+++ setup/common/delete-from-saved-settings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -f
 export SETTINGS=$HOME/hk8sLabsSettings
 
-texttodelete=$1
+TEXT_TO_DELETE=$1
 
-if [ -z $texttodeletev ]
+if [ -z $TEXT_TO_DELETE ]
 then
   echo "Cannot remove blank contents from the settiongs"
   exit 1
@@ -11,8 +11,8 @@ fi
 
 TMP_SETTINGS=$SETTINGS.tmp
 
-echo removing $texttodelete from $SETTINGS
-cat $SETTINGS | grep -v $texttodelete > $TMP_SETTINGS
+echo removing $TEXT_TO_DELETE from $SETTINGS
+cat $SETTINGS | grep -v $TEXT_TO_DELETE > $TMP_SETTINGS
 
 rm $SETTINGS
 mv $TMP_SETTINGS $SETTINGS
