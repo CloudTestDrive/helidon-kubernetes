3069d785028627ba7aa357ffe719221d7eec8a82
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-02-22 13:14:36 +0000

    script path fix

--- setup/devops-labs/vault-secrets-setup.sh
+++ setup/devops-labs/vault-secrets-setup.sh
@@ -46,7 +46,7 @@ then
   echo "No OCIR host variable for the storefront image set, have you run the image-environment-setup.sh or ocir-setup.sh script ?"
   echo "Cannot continue setting up this secret"
 else
-  bash ../common/vault-secrets/vault-individual-secret-setup.sh OCIR_HOST 'OCIR hostname' $OCIR_STOREFRONT_LOCATION
+  bash ./vault-individual-secret-setup.sh OCIR_HOST 'OCIR hostname' $OCIR_STOREFRONT_LOCATION
 fi
 
 # the object storage namespace should exist as it's a tenancy level property, so no need to check
