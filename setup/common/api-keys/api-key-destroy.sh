344f0d28ecc2bee5141b85f8972ac70b55a89b51
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-02-24 20:13:57 +0000

    correctly cleans up on reuse

--- setup/common/api-keys/api-key-destroy.sh
+++ setup/common/api-keys/api-key-destroy.sh
@@ -10,22 +10,33 @@ if [ -f $SETTINGS ]
     exit 10
 fi
 
-if [ -z $USER_OCID ]
+if [ -z "$USER_OCID" ]
 then
   echo 'No user ocid, unable to continue - have you run the user-identity-setup.sh script ?'
   exit 1
 fi
-if [ -z $API_KEY_REUSED ]
+if [ -z "$API_KEY_REUSED" ]
 then
   echo "No saved API key information, cannot safely proceed."
   exit 2
 else
-  echo "API key was created by these scripts, proceeding"
+  echo "API key reuse info found, proceeding"
 fi
-if [ -z $API_KEY_FINGERPRINT ]
+
+if [ "$API_KEY_REUSED" = true ]
+then
+  echo "API Key was not created by this script, cannot continue."
+  bash ../delete-from-saved-settings.sh API_KEY_FINGERPRINT
+  bash ../delete-from-saved-settings.sh API_KEY_REUSED
+  exit 3
+else
+  echo "API key reuse info found, proceeding"
+fi
+
+if [ -z "$API_KEY_FINGERPRINT" ]
 then
   echo "No saved API key fingerprint, cannot proceed."
-  exit 2
+  exit 4
 else
   echo "Found API Key fingerprint"
 fi
