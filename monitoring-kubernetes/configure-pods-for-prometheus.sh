f553ebfb3e48f524b0c9dd92e114a805e30bb935
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-02-24 17:40:43 +0000

    fit typo in path name

--- monitoring-kubernetes/configure-pods-for-prometheus.sh
+++ monitoring-kubernetes/configure-pods-for-prometheus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -f
-DEPLOY_DIR=$HOME/helidon-kubernertes
+DEPLOY_DIR=$HOME/helidon-kubernetes
 cat $DEPLOY_DIR/stockmanager-deployment.yaml | sed -e 's/ #  p/   p/' | sed -e 's/ #a/ a/' > $DEPLOY_DIR/tmp
 mv $DEPLOY_DIR/tmp $DEPLOY_DIR/stockmanager-deployment.yaml
 kubectl apply -f $DEPLOY_DIR/stockmanager-deployment.yaml
