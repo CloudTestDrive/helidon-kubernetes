f18762bc53b2b51d9185043388ebf62bf86ca5e0
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-02-25 14:20:55 +0000

    quoting updates

--- setup/devops-labs/security-setup.sh
+++ setup/devops-labs/security-setup.sh
@@ -4,10 +4,10 @@ export SETTINGS=$HOME/hk8sLabsSettings
 
 if [ -f $SETTINGS ]
   then
-    echo Loading existing settings information
+    echo "Loading existing settings information"
     source $SETTINGS
   else 
-    echo No existing settings cannot contiue
+    echo "No existing settings cannot contiue"
     exit 10
 fi
 
@@ -15,13 +15,13 @@ source $SETTINGS
 
 if [ -z $COMPARTMENT_OCID ]
 then
-  echo Your COMPARTMENT_OCID has not been set, you need to run the compartment-setup.sh before you can run this script
+  echo "Your COMPARTMENT_OCID has not been set, you need to run the compartment-setup.sh before you can run this script"
   exit 2
 fi
 
 if [ -z $USER_INITIALS ]
 then
-  echo Your USER_INITIALS has not been set, you need to run the initials-setup.sh before you can run this script
+  echo "Your USER_INITIALS has not been set, you need to run the initials-setup.sh before you can run this script"
   exit 2
 fi
 
