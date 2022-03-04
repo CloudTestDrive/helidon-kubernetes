1c1d80abb513c7576705b75afb64bfe0cfcb466f
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-03-04 13:01:07 +0000

    switch to using RSA keys

--- setup/kubernetes-labs/create-store-cert.sh
+++ setup/kubernetes-labs/create-store-cert.sh
@@ -3,7 +3,7 @@ echo removing existing certs
 touch tls-store-$1.crt tls-store-$1.key
 rm tls-store-$1.crt tls-store-$1.key
 echo creating tls secret using step with common name of store.$1.nip.io
-$HOME/keys/step certificate create store.$1.nip.io tls-store-$1.crt tls-store-$1.key --profile leaf --not-after 8760h --no-password --insecure --ca $HOME/keys/root.crt --ca-key $HOME/keys/root.key
+$HOME/keys/step certificate create store.$1.nip.io tls-store-$1.crt tls-store-$1.key --profile leaf --not-after 8760h --no-password --insecure  --kty=RSA --ca $HOME/keys/root.crt --ca-key $HOME/keys/root.key
 echo removing any existing tls-store secret
 kubectl delete secret tls-store --ignore-not-found=true
 echo creating new tls-store secret
