0244ece0e8038daa3aa424e8e0186749bb459a88
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-03-15 11:27:13 +0000

    better description of compartment hierarchy

--- setup/common/compartment-setup.sh
+++ setup/common/compartment-setup.sh
@@ -49,12 +49,12 @@ then
   then
     PARENT_NAME="Tenancy root"
   else 
-    PARENT_NAME="$COMPARTMENT_PARENT_NAME sub compartment"
+    PARENT_NAME="$COMPARTMENT_PARENT_NAME compartment"
   fi
 
-  echo "This script will create a compartment called $COMPARTMENT_NAME for you if it doesn't exist, this will be in the $PARENT_NAME. If a compartment with the same name already exists you can re-use change the name to create or re-use a different compartment."
+  echo "This script will create a sub compartment called $COMPARTMENT_NAME for you if it doesn't exist, this will be in the $PARENT_NAME. If a sub compartment with the same name already exists in $PARENT_NAME you can re-use change the name to create or re-use a different compartment."
 
-  echo "If you want to use somewhere different from $PARENT_NAME as the parent of the compartment you are about to create (or re-use) then enter n, if you want to use $PARENT_NAME for your parent then enter y"
+  echo "If you want to use somewhere different from $PARENT_NAME as the parent of the sub compartment you are about to create (or re-use) then enter n, if you want to use $PARENT_NAME for your parent then enter y"
   read -p "Use the $PARENT_NAME (y/n) ? " REPLY
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
@@ -65,19 +65,19 @@ then
     exit 1
   fi
 
-  echo "We are going to create or if it already exists reuse use a compartment called $COMPARTMENT_NAME in $PARENT_NAME, if you want you can change the compartment name from $COMPARTMENT_NAME - this is not recommended and you will need to remember to use a different name in the lab." 
+  echo "We are going to create or if it already exists reuse use a sub compartment called $COMPARTMENT_NAME in $PARENT_NAME, if you want you can change the sub compartment name from $COMPARTMENT_NAME - this is not recommended and you will need to remember to use a different name in the lab." 
   read -p "Do you want to use $COMPARTMENT_NAME as the compartment name  (y/n) ? " REPLY
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
-    echo "OK, this isn't the best of ideas, please enter the new name for your compartment, it must be a single word"
+    echo "OK, this isn't the best of ideas, please enter the new name for your sub compartment, it must be a single word"
     read COMPARTMENT_NAME
     if [ -z "$COMPARTMENT_NAME" ]
     then
-      echo "You do actually need to enter the new name for the compartment, exiting"
+      echo "You do actually need to enter the new name for the sub compartment, exiting"
       exit 1
     fi
   else     
-    echo "OK, going to use $COMPARTMENT_NAME as the compartment name"
+    echo "OK, going to use $COMPARTMENT_NAME as the sub compartment name"
   fi
 
   # OK, actual do the work.
@@ -92,19 +92,19 @@ then
     COMPARTMENT_OCID=`oci iam compartment create --name $COMPARTMENT_NAME --compartment-id $COMPARTMENT_PARENT_OCID --description "Labs compartment" --wait-for-state ACTIVE --wait-interval-seconds 10 | jq -j '.data.id'`
     if [ -z $COMPARTMENT_OCID ]
     then
-      echo "The compartment has not been created for some reason, cannot continue"
+      echo "The sub compartment $COMPARTMENT_NAME has not been created for some reason, cannot continue"
       exit 3
     fi
-    echo "Created compartment $COMPARTMENT_NAME in $PARENT_NAME It's OCID is $COMPARTMENT_OCID"
+    echo "Created sub compartment $COMPARTMENT_NAME in $PARENT_NAME It's OCID is $COMPARTMENT_OCID"
     echo "COMPARTMENT_OCID=$COMPARTMENT_OCID" >> $SETTINGS
     echo "COMPARTMENT_REUSED=false" >> $SETTINGS
-    echo "It may take a short while before new compartment has propogated and the web UI reflects this"
+    echo "It may take a short while before new sub compartment has propogated and the web UI reflects this"
     # Wait for the compartment to exist, there have been cases where the --wait-for-state has not worked and that had broken downstream stages
     COMPARTMENT_STATUS="WATTING"
-    echo "Checking for active compartment state"
+    echo "Checking for active sub compartment state"
     while [ "$COMPARTMENT_STATUS"  !=  "ACTIVE" ]
     do
-      echo "Retrieving compartment state"
+      echo "Retrieving sub compartment state"
       COMPARTMENT_STATUS_RESP=`oci iam compartment get --compartment-id $COMPARTMENT_OCID 2>&1 | grep -v "ServiceError"`
       # echo "Returned info is \n$COMPARTMENT_STATUS_RESP"
       COMPARTMENT_STATUS_CODE=`echo $COMPARTMENT_STATUS_RESP | jq -j '.status'`
@@ -126,22 +126,22 @@ then
       # echo "Status is $COMPARTMENT_STATUS"
       if [ "$COMPARTMENT_STATUS" = "ACTIVE" ]
       then
-        echo "Compartment is active, continuing"
+        echo "Sub compartment is active, continuing"
       else
-        echo "Waiting for active compartment state"
+        echo "Waiting for active sub compartment state"
         sleep 5
       fi
     done
-    echo "Compartment $COMPARTMENT_NAME in $PARENT_NAME is now ready for use"
+    echo "Sub compartment $COMPARTMENT_NAME in $PARENT_NAME is now ready for use"
   else
-    echo "Compartment $COMPARTMENT_NAME already exists in $PARENT_NAME, do you want to re-use it (y/n) ?"
+    echo "Sub compartment $COMPARTMENT_NAME already exists in $PARENT_NAME, do you want to re-use it (y/n) ?"
     read CONFIRM
     if [[ ! $CONFIRM =~ ^[Yy]$ ]]
     then
-      echo "OK, This script is about to exit, re-run it entering a compartment name different from $COMPARTMENT_NAME"
+      echo "OK, This script is about to exit, re-run it entering a name for the sub compartment which is different from $COMPARTMENT_NAME"
       exit 1
     else
-      echo "OK, going to reuse compartment $COMPARTMENT_NAME in $PARENT_NAME" 
+      echo "OK, going to reuse sub compartment $COMPARTMENT_NAME in $PARENT_NAME" 
       echo "COMPARTMENT_OCID=$COMPARTMENT_OCID" >> $SETTINGS
       echo "COMPARTMENT_REUSED=true" >> $SETTINGS
     fi
@@ -151,11 +151,11 @@ else
   COMPARTMENT_NAME=`oci iam compartment get  --compartment-id $COMPARTMENT_OCID | jq -j '.data.name'`
   if [ -z $COMPARTMENT_NAME ]
   then
-    echo "Unable to locate compartment for provided OCID $COMPARTMENT_OCID"
+    echo "Unable to locate sub compartment for provided OCID $COMPARTMENT_OCID"
     echo "Please check that the value of COMPARTMENT_OCID in $SETTINGS is correct if nor remove or replace it"
     exit 5
   else
-    echo "Located compartment named $COMPARTMENT_NAME with pre-specified OCID of $COMPARTMENT_OCID, will use this compartment"
+    echo "Located sub compartment named $COMPARTMENT_NAME with pre-specified OCID of $COMPARTMENT_OCID, will use this compartment"
     # Flag this as reused and refuse to destroy it
     echo "COMPARTMENT_REUSED=true" >> $SETTINGS
   fi
