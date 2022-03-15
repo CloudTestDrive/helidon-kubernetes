0a89570a86d49bb6aa2e1bce36384d26905dfd94
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-03-15 11:17:42 +0000

    better diags on deletion of key and vault

--- setup/common/vault-destroy.sh
+++ setup/common/vault-destroy.sh
@@ -41,11 +41,12 @@ else
     else
       echo "Scheduling deletion of key, this will normally take effect in a months time"
       echo "should you with to cancel during that time you can do so."
-      echo "While deletion is pending any secretes encrypted by this key will be unavailable unless"
-      echo "The key deletion is cancled, this will also require that you cancel the vault deletion"
+      echo "While deletion is pending any secretes encrypted by this key will be unavailable unless the"
+      echo "key deletion is cancled, this will also require that you cancel the vault deletion"
       echo "as you can't have a active key in a vault that is itself pending deletion"
       echo "Note, a key that is pending deletion will prevent the compartment that contains it from being deleted"
-      oci kms management key schedule-deletion --key-id $VAULT_KEY_OCID --endpoint $VAULT_ENDPOINT
+      NEW_KEY_STATE=`oci kms management key schedule-deletion --key-id $VAULT_KEY_OCID --endpoint $VAULT_ENDPOINT | jq -j '.data."lifecycle-state"'`
+      echo "Keys lifecycle state is $NEW_KEY_STATE" 
       echo "Removing details from the settings file"
       bash ./delete-from-saved-settings.sh VAULT_KEY_OCID
       bash ./delete-from-saved-settings.sh VAULT_KEY_REUSED
@@ -70,7 +71,8 @@ else
       echo "While deletion is pending any secretes in this vault will be unavailable unless"
       echo "The vault deletion is cancled"
       echo "Note, a vault that is pending deletion will prevent the compartment that contains it from being deleted"
-      oci kms management vault schedule-deletion --vault-id $VAULT_OCID
+      NEW_VAULT_STATE=`oci kms management vault schedule-deletion --vault-id $VAULT_OCID | jq -j '.data."lifecycle-state"'`
+      echo "Vaults lifecycle state is $NEW_VAULT_STATE" 
       echo "Removing details from the settings file"
       bash ./delete-from-saved-settings.sh VAULT_OCID
       bash ./delete-from-saved-settings.sh VAULT_REUSED
