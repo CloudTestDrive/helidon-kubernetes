632aaea1725b7b4de38f80b27289c57d0a6c64a9
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-03-16 12:19:29 +0000

    better not found image checking

--- setup/common/container-image-destroy.sh
+++ setup/common/container-image-destroy.sh
@@ -38,16 +38,24 @@ OCIR_STOCKMANAGER_NAME=`oci artifacts  container repository get  --repository-id
 
 bash stockmanager-deployment-update.sh reset $OCIR_STOCKMANAGER_LOCATION $OBJECT_STORAGE_NAMESPACE $OCIR_STOCKMANAGER_NAME
 
-IMAGE_STOCKMANAGER_V001=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOCKMANAGER_NAME:0.0.1 | jq -r ".data.items[0].id"`
+IMAGE_STOCKMANAGER_V001=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOCKMANAGER_NAME:0.0.1 | jq -j ".data.items[0].id"`
 if [ -z $IMAGE_STOCKMANAGER_V001 ]
+then
+  IMAGE_STOCKMANAGER_V001="null"
+fi
+if [ "$IMAGE_STOCKMANAGER_V001" = "null" ]
 then
   echo "Cant locate 0.0.1 stock manager image, skipping"
 else
   oci artifacts container image delete --force --image-id $IMAGE_STOCKMANAGER_V001
   echo "Removed stockmanager 0.0.1"
 fi
-IMAGE_STOCKMANAGER_V001=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOCKMANAGER_NAME:0.0.2 | jq -r ".data.items[0].id"`
+IMAGE_STOCKMANAGER_V002=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOCKMANAGER_NAME:0.0.2 | jq -j ".data.items[0].id"`
 if [ -z $IMAGE_STOCKMANAGER_V002 ]
+then
+  IMAGE_STOCKMANAGER_V002="null"
+fi
+if [ "$IMAGE_STOCKMANAGER_V002" = "null" ]
 then
   echo "Cant locate 0.0.2 stock manager image, skipping"
 else
@@ -61,16 +69,24 @@ OCIR_STOREFRONT_NAME=`oci artifacts  container repository get  --repository-id $
 
 bash storefront-deployment-update.sh reset $OCIR_STOREFRONT_LOCATION $OBJECT_STORAGE_NAMESPACE $OCIR_STOREFRONT_NAME
 
-IMAGE_STOREFRONT_V001=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOREFRONT_NAME:0.0.1 | jq -r ".data.items[0].id"`
+IMAGE_STOREFRONT_V001=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOREFRONT_NAME:0.0.1 | jq -j ".data.items[0].id"`
 if [ -z $IMAGE_STOREFRONT_V001 ]
+then
+  IMAGE_STOREFRONT_V001="null"
+fi
+if [ "$IMAGE_STOREFRONT_V001"="null" ]
 then
   echo "Cant locate 0.0.1 storefront image, skipping"
 else
   oci artifacts container image delete --force  --image-id $IMAGE_STOREFRONT_V001
   echo "Removed storefront 0.0.1"
 fi
-IMAGE_STOREFRONT_V001=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOREFRONT_NAME:0.0.2 | jq -r ".data.items[0].id"`
+IMAGE_STOREFRONT_V002=`oci artifacts container image list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_STOREFRONT_NAME:0.0.2 | jq -j ".data.items[0].id"`
 if [ -z $IMAGE_STOREFRONT_V002 ]
+then
+  IMAGE_STOREFRONT_V002="null"
+fi
+if [ "$IMAGE_STOREFRONT_V002"="null" ]
 then
   echo "Cant locate 0.0.2 storefront image, skipping"
 else
