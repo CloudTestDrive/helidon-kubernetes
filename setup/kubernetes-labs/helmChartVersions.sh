0f27a348d73b51dcdc083deca99b50fbd8dad12c
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-06-08 13:51:41 +0100

    helm chart version updates

--- setup/kubernetes-labs/helmChartVersions.sh
+++ setup/kubernetes-labs/helmChartVersions.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-ingressHelmChartVersion=4.1.0
-dashboardHelmChartVersion=5.4.1
+#ingressHelmChartVersion=4.1.0
+#dashboardHelmChartVersion=5.4.1
+ingressHelmChartVersion=4.1.3
+dashboardHelmChartVersion=5.7.0
+prometheusHelmChartVersion=14.4.1
+grafanaHelmChartVerion=6.29.6
