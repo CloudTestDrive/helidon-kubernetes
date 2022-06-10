a5d3683492e54070f4545c7782b6570c6b2b58ef
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-06-10 12:20:02 +0100

    make the prometheus annotation enabling more selective

--- monitoring-kubernetes/configure-pods-for-prometheus.sh
+++ monitoring-kubernetes/configure-pods-for-prometheus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -f
 DEPLOY_DIR=$HOME/helidon-kubernetes
-cat $DEPLOY_DIR/stockmanager-deployment.yaml | sed -e 's/ #  p/   p/' | sed -e 's/ #a/ a/' > $DEPLOY_DIR/tmp
+cat $DEPLOY_DIR/stockmanager-deployment.yaml | sed -e 's/ #  prometheus.io/   prometheus.io/' | sed -e 's/ #annotations/ annotations/' > $DEPLOY_DIR/tmp
 mv $DEPLOY_DIR/tmp $DEPLOY_DIR/stockmanager-deployment.yaml
 kubectl apply -f $DEPLOY_DIR/stockmanager-deployment.yaml
-cat $DEPLOY_DIR/storefront-deployment.yaml | sed -e 's/ #  p/   p/' | sed -e 's/ #a/ a/' > $DEPLOY_DIR/tmp
+cat $DEPLOY_DIR/storefront-deployment.yaml | sed -e 's/ #  prometheus.io/   prometheus.io/' | sed -e 's/ #annotations/ annotations/' > $DEPLOY_DIR/tmp
 mv $DEPLOY_DIR/tmp $DEPLOY_DIR/storefront-deployment.yaml
 kubectl apply -f $DEPLOY_DIR/storefront-deployment.yaml
