16a8333c0810fdb65a2f44f57a2af017fab6a1b1
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-06-10 16:51:56 +0100

    updates

--- setup/kubernetes-labs/optional-labs/logs-to-elastic-setup.sh
+++ setup/kubernetes-labs/optional-labs/logs-to-elastic-setup.sh
@@ -45,10 +45,10 @@ helm install elasticsearch elastic/elasticsearch --namespace logging --version $
 echo "Installing fluentd daemon set"
 kubectl apply -f fluentd-daemonset-elasticsearch-rbac.yaml
 
-TODAYS_DATE=`data +'%Y-$m-%d`
+TODAYS_DATE=`date +'%Y-%m-%d'`
 echo "Access elastic records for today at :"
-echo "https://search.logging.$EXTERANAL_IP.nip.io/logstash-$TODAYS_DATE/_search"
+echo "https://search.logging."$EXTERNAL_IP".nip.io/logstash-"$TODAYS_DATE"/_search"
 echo "Access elastic records for storefront today at :"
-echo "at https://search.logging.$EXTERANAL_IP.nip.io/logstash-$TODAYS_DATE/_search?q=kubernetes.container_name:storefront"
+echo "at https://search.logging."$EXTERNAL_IP".nip.io/logstash-"$TODAYS_DATE"/_search?q=kubernetes.container_name:storefront"
 echo "auth user admin and password $PASSWORD"
 
