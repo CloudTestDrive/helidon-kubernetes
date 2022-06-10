8f9f36855b5befbd7bf83ea28dbe9ab1362992bd
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-06-10 12:06:43 +0100

    delete script now removes certificate files

--- setup/kubernetes-labs/monitoring-destroy.sh
+++ setup/kubernetes-labs/monitoring-destroy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -f
 echo "Destroying monitoring namespace"
 kubectl delete namespace monitoring  --ignore-not-found=true
+
+echo "Removing Certificates"
+cd $HOME/helidon-kubernetes/monitoring-kubernetes
+rm tls-*.crt
+rm tls-*.key
