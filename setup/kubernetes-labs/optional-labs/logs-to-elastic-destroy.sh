5af29ba3014b0089d43a8bb666d645e0a77e178c
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-06-15 17:42:33 +0100

    load the settings

--- setup/kubernetes-labs/optional-labs/logs-to-elastic-destroy.sh
+++ setup/kubernetes-labs/optional-labs/logs-to-elastic-destroy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -f
 
+export SETTINGS=$HOME/hk8sLabsSettings
+
+if [ -f $SETTINGS ]
+  then
+    echo "Loading existing settings information"
+    source $SETTINGS
+  else 
+    echo "No existing settings cannot continue"
+    exit 10
+fi
+
 
 echo "Destroying logging namespace"
 kubectl delete namespace logging  --ignore-not-found=true
