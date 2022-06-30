8b36b6bca24f9a9711cbf432ddf04b1ddaed6253
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-06-30 13:12:39 +0100

    correct the parallel check to look for DB_OCID not ATPDB_OCID

--- setup/common/wait-for-common-services.sh
+++ setup/common/wait-for-common-services.sh
@@ -15,13 +15,13 @@ OKE_OCID_NAME=OKE_OCID_$CLUSTER_CONTEXT_NAME
 echo "Waiting for core services to be available." 
 export WAIT_LOOP_COUNT=180
 
-bash ./wait-for-service-availability.sh ATPDB_OCID $OKE_OCID_NAME IMAGES_READY
+bash ./wait-for-service-availability.sh DB_OCID $OKE_OCID_NAME IMAGES_READY
 
 RESP=$?
 
 if [ $RESP -ne 0 ]
 then
-  echo "One of more of the services ATPDB_OCID $OKE_OCID_NAME IMAGES_READY did not start within $WAIT_LOOP_COUNT test loops"
+  echo "One of more of the services DB_OCID $OKE_OCID_NAME IMAGES_READY did not start within $WAIT_LOOP_COUNT test loops"
   echo "Cannot continue"
   exit $RESP
 fi
