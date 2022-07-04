94efad0584052611917d95b8fb9b188f949482bc
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-04 09:39:10 +0100

    delete now used the combined policy

--- setup/kubernetes-labs/opensearch/opensearch-policy-destroy.sh
+++ setup/kubernetes-labs/opensearch/opensearch-policy-destroy.sh
@@ -17,16 +17,13 @@ if [ -z "$OPENSEARCH_POLICIES_CONFIGURED" ]
 then
   echo "OpenSearch policies not configured"
   exit 0
-else
+els
   echo "Removing configured OpenSearch policies"
 fi
 
 cd ../../common/policies
 
-bash ./policy-destroy.sh "$USER_INITIALS"OpenSearchVNICPolicy
-bash ./policy-destroy.sh "$USER_INITIALS"OpenSearchSubnetsPolicy
-bash ./policy-destroy.sh "$USER_INITIALS"OpenSearchNSGPolicy
-bash ./policy-destroy.sh "$USER_INITIALS"OpenSearchSVCNSPolicy
+bash ./policy-destroy.sh "$USER_INITIALS"OpenSearchPolicy
 
 
 # delete script is in common, we are in common/policies
