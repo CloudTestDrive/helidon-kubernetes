a7dfe6a54caebe5e28415fa723b9a07e7944bbb2
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-07 20:01:02 +0100

    remove duplicate reuse check

--- setup/devops-labs/dynamic-groups-setup.sh
+++ setup/devops-labs/dynamic-groups-setup.sh
@@ -13,16 +13,6 @@ fi
 
 source $SETTINGS
 
-
-if [ -z "$DEVOPS_DYNAMIC_GROUPS_CONFIGURED" ]
-then
-  echo "Dynamic groups not configured, setting up"
-else
-  echo "Dynamic groups already configured"
-  exit 0
-fi
-
-
 if [ -z $COMPARTMENT_OCID ]
 then
   echo "Your COMPARTMENT_OCID has not been set, you need to run the compartment-setup.sh before you can run this script"
@@ -37,7 +27,7 @@ fi
 
 if [ -z "$DEVOPS_DYNAMIC_GROUPS_CONFIGURED" ]
 then
-  echo "Dynamic groups not yet configured"
+  echo "Dynamic groups not yet configured, setting up"
 else
   echo "Dynamic groups have already been configured"
   exit 0
