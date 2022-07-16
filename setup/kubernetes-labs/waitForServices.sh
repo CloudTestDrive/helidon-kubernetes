5e942c0f48593806e9520b679a12a952bd4835e1
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-16 19:06:20 +0100

    count the wait process up to a max

--- setup/kubernetes-labs/waitForServices.sh
+++ setup/kubernetes-labs/waitForServices.sh
@@ -8,8 +8,19 @@ fi
 echo checking https://store.$1.nip.io/store/stocklevel for a 200 response
 
 resp=""
+WAIT_LOOP_LIMIT=60
+checkcounter=0
 while [ -z "$resp" ] ; do
-  echo "Waiting for services to start"
+  let checkcounter="$checkcounter+1"
+  echo "Waiting for services to start, test $checkcounter"
   resp=$(curl -i -X GET -u jack:password -k -s https://store.$1.nip.io/store/stocklevel | grep "200 OK")
-  [ -z "$resp" ] && sleep 10
+  if [ -z "$resp" ] 
+  then
+    if [ "$checkcounter" = "$WAIT_LOOP_LIMIT" ]
+    then
+      echo "Hit the wait limit, sorry, cannot continue"
+      exit 1
+    fi
+    sleep 10
+  fi
 done
