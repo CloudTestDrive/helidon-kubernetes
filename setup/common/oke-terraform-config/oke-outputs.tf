f0442d80d57332f4d819c8d4f01fb868479aa21f
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-17 14:56:22 +0100

    switch the outputs module

--- setup/common/oke-terraform-config/oke-outputs.tf
+++ setup/common/oke-terraform-config/oke-outputs.tf
@@ -8,15 +8,15 @@ output "vcn_id" {
 }
 output "subnet_ids" {
   description = "map of subnet ids (worker, int_lb, pub_lb) used by OKE."
-  value       = module.network.subnet_ids
+  value       = module.oke.subnet_ids
 }
 output "int_lb_nsg" {
   description = "id of default NSG that can be associated with the internal load balancer"
-  value       = module.network.int_lb
+  value       = module.oke.int_lb
 }
 output "pub_lb_nsg" {
   description = "id of default NSG that can be associated with the internal load balancer"
-  value       = module.network.pub_lb
+  value       = module.oke.pub_lb
 }
 output "kubeconfig" {
   description = "convenient command to set KUBECONFIG environment variable before running kubectl locally"
