99d9d1cf66cc244071d6063e68b648273bae919d
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-21 19:05:31 +0100

    now understand about different kubectl contexts

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+SCRIPT_NAME=`basename $0`
+CLUSTER_CONTEXT_NAME=one
+if [ $# -ge 1 ]
+then
+  CLUSTER_CONTEXT_NAME=$1
+  echo "$SCRIPT_NAME Operating on context name $CLUSTER_CONTEXT_NAME"
+else
+  echo "$SCRIPT_NAME Using default context name of $CLUSTER_CONTEXT_NAME"
+fi
 echo Creating zipkin deployment
-kubectl apply -f zipkin-deployment.yaml --record=true
+kubectl apply -f zipkin-deployment.yaml --record=true  --context $CLUSTER_CONTEXT_NAME
 echo Creating stockmanager deployment
-kubectl apply -f stockmanager-deployment.yaml --record=true
+kubectl apply -f stockmanager-deployment.yaml --record=true --context $CLUSTER_CONTEXT_NAME
 echo Creating storefront deployment
-kubectl apply -f storefront-deployment.yaml --record=true
+kubectl apply -f storefront-deployment.yaml --record=true --context $CLUSTER_CONTEXT_NAME
 echo Kubenetes config is
-kubectl get all
+kubectl get all --context $CLUSTER_CONTEXT_NAME
