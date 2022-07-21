04c301ffa4ffd302d8b098e3d0c6f91419d8506f
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-21 14:05:47 +0100

    fix the templating output name

--- setup/kubernetes-labs/ingressrules/template-ingress.sh
+++ setup/kubernetes-labs/ingressrules/template-ingress.sh
@@ -22,9 +22,9 @@ then
 else
   echo "$SCRIPT_NAME  Using default context name of $CLUSTER_CONTEXT_NAME"
 fi
-for INGRESS_TEMPLATE_FILE in $INGRESS_DIR/ingress*Rules.yaml 
+for INGRESS_TEMPLATE_FILE in "$INGRESS_DIR/ingress*Rules.yaml"
 do
    DEST_FILE=`echo $INGRESS_TEMPLATE_FILE | sed -e "s/Rules/Rules-$CLUSTER_CONTEXT_NAME/"`
-   echo "Templating $INGRESS_TEMPLATE_FILE  to $destfile"
+   echo "Templating $INGRESS_TEMPLATE_FILE  to $DEST_FILE"
    bash $HOME/helidon-kubernetes/setup/common/template-file.sh $INGRESS_TEMPLATE_FILE $DEST_FILE $OLD_TEXT.nip.io $NEW_TEXT.nip.io
 done
