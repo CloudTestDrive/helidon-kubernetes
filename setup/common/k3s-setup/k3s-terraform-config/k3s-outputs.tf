21d04d381ab0d5ccecd0602e9b68054e08f82eb1
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-22 19:09:46 +0100

    another output try

--- setup/common/k3s-setup/k3s-terraform-config/k3s-outputs.tf
+++ setup/common/k3s-setup/k3s-terraform-config/k3s-outputs.tf
@@ -1,4 +1,4 @@
 output "bastion_public_ip" {
   description = "public ip address of bastion host"
-  value       = local.bastion_public_ip
+  value       = modules.terraform-oci-k3s.bastion_public_ip
 }
