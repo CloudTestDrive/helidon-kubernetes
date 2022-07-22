aa5a58b89abf42f0eb58f1583bc65ed3bada0a3b
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-22 19:08:42 +0100

    even simpler test outputs

--- setup/common/k3s-setup/k3s-terraform-config/k3s-outputs.tf
+++ setup/common/k3s-setup/k3s-terraform-config/k3s-outputs.tf
@@ -2,8 +2,3 @@ output "bastion_public_ip" {
   description = "public ip address of bastion host"
   value       = local.bastion_public_ip
 }
-
-output "ssh_to_server" {
-  description = "convenient command to ssh to the server host"
-  value       = "ssh -i ${var.ssh_private_key_path} -J opc@${local.bastion_public_ip} opc@${local.server_private_ip}"
-}
