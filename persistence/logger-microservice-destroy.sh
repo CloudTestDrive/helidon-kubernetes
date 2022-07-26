9b783ed263f9ccd313eedbe09848a8353a800220
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-26 18:12:44 +0100

    delete actually deletes the resouerce now, no longer applies them !

--- persistence/logger-microservice-destroy.sh
+++ persistence/logger-microservice-destroy.sh
@@ -14,14 +14,14 @@ echo "Updating the storefront config and restarting"
 bash update-storefront-configmap.sh reset $CLUSTER_CONTEXT_NAME
 # This assumes that the ingress rules and deployment have been updated
 echo "Delete the ingress rule"
-kubectl apply --context $CLUSTER_CONTEXT_NAME -f ingressLoggerRules-"$CLUSTER_CONTEXT_NAME".yaml
+kubectl delete --context $CLUSTER_CONTEXT_NAME -f ingressLoggerRules-"$CLUSTER_CONTEXT_NAME".yaml
 echo "Delete the deployment"
-kubectl apply --context $CLUSTER_CONTEXT_NAME -f logger-deployment.yaml
+kubectl delete --context $CLUSTER_CONTEXT_NAME -f logger-deployment.yaml
 echo "Delete logger service"
-kubectl apply --context $CLUSTER_CONTEXT_NAME -f serviceLogger.yaml
+kubectl delete --context $CLUSTER_CONTEXT_NAME -f serviceLogger.yaml
 echo "Delete logger configmaps"
 bash delete-configmaps.sh $CLUSTER_CONTEXT_NAME
 echo "Delete logger secrets"
 bash delete-secrets.sh $CLUSTER_CONTEXT_NAME
 echo "Delete persistent volume for the logger"
-kubectl apply --context $CLUSTER_CONTEXT_NAME -f persistentVolumeClaim.yaml
+kubectl delete --context $CLUSTER_CONTEXT_NAME -f persistentVolumeClaim.yaml
