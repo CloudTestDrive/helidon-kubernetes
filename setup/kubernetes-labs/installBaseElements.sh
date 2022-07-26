1b51ccea9bf28526752122812a1b774d3883fd46
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-26 18:09:11 +0100

    info output now includes commands to retrueve the log data

--- setup/kubernetes-labs/installBaseElements.sh
+++ setup/kubernetes-labs/installBaseElements.sh
@@ -124,6 +124,9 @@ echo "status command"  >> $INFO_FILE
 echo "curl -i -X GET -k $BASE_URL/sf/status"  >> $INFO_FILE
 echo >> $INFO_FILE
 
+echo "Test reserve stock command  - Assumes that you have resources called Pins"   >> $INFO_FILE
+echo 'curl -u jack:password -i -H "Content-Type: application/json" -X POST -d "{\"requestedItem\":\"Pins\", \"requestedCount\": 5}"'" http://$BASE_URL/store/reserveStock"  >> $INFO_FILE
+echo >> $INFO_FILE
 
 # now we have the ingress we csan update the rules to fit it
 echo "updating base ingress rules"
