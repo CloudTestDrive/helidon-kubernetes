7fbd51897e0bc227b32e34948a09dbebb2f133d0
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-27 19:14:35 +0100

    now checks based on the type of cluster, not the OCID as non OKE clusters don't have an OCID

--- setup/common/wait-for-common-services.sh
+++ setup/common/wait-for-common-services.sh
@@ -9,19 +9,18 @@ then
 else
   echo "Using default context name of $CLUSTER_CONTEXT_NAME"
 fi
-
-OKE_OCID_NAME=`bash ./settings/to-valid-name.sh OKE_OCID_$CLUSTER_CONTEXT_NAME`
+KUBERNETES_CLUSTER_TYPE_NAME=`bash settings/to-valid-name.sh "KUBERNETES_CLUSTER_TYPE_"$CLUSTER_CONTEXT_NAME`
 
 echo "Waiting for core services to be available." 
 export WAIT_LOOP_COUNT=180
 
-bash ./wait-for-service-availability.sh DB_OCID $OKE_OCID_NAME IMAGES_READY
+bash ./wait-for-service-availability.sh DB_OCID $KUBERNETES_CLUSTER_TYPE_NAME IMAGES_READY
 
 RESP=$?
 
 if [ $RESP -ne 0 ]
 then
-  echo "One of more of the services DB_OCID $OKE_OCID_NAME IMAGES_READY did not start within $WAIT_LOOP_COUNT test loops"
+  echo "One of more of the services DB_OCID $KUBERNETES_CLUSTER_TYPE_NAME IMAGES_READY did not start within $WAIT_LOOP_COUNT test loops"
   echo "Cannot continue"
   exit $RESP
 fi
