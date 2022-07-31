1ee8e4929d1edec9bed82f34264b9db3bfb221fe
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-07-31 18:29:22 +0100

    try and pre-configure the prometheus data source in grafana so don;t need to do it by hand post event

--- setup/kubernetes-labs/optional-labs/monitoring-setup.sh
+++ setup/kubernetes-labs/optional-labs/monitoring-setup.sh
@@ -35,10 +35,10 @@ echo "Creating namespace in cluster $CLUSTER_CONTEXT_NAME"
 kubectl create namespace monitoring --context $CLUSTER_CONTEXT_NAME
 
 echo "Creating Prometheus auth details"
-htpasswd -c -b auth admin $PROMETHEUS_PASSWORD
+htpasswd -c -b auth.$CLUSTER_CONTEXT_NAME admin $PROMETHEUS_PASSWORD
 
 echo "Creating Prometheus auth secret in cluster $CLUSTER_CONTEXT_NAME"
-kubectl create secret generic web-ingress-auth -n monitoring --from-file=auth --context $CLUSTER_CONTEXT_NAME
+kubectl create secret generic web-ingress-auth -n monitoring --from-file=auth.$CLUSTER_CONTEXT_NAME --context $CLUSTER_CONTEXT_NAME
 
 echo "Create Prometheus certificate"
 $HOME/keys/step certificate create prometheus.monitoring.$EXTERNAL_IP.nip.io tls-prometheus-$EXTERNAL_IP.crt tls-prometheus-$EXTERNAL_IP.key --profile leaf  --not-after 8760h --no-password --insecure --kty=RSA --ca $HOME/keys/root.crt --ca-key $HOME/keys/root.key
@@ -59,7 +59,7 @@ echo "Creating Grafana certificate secret in cluster $CLUSTER_CONTEXT_NAME"
 kubectl create secret tls tls-grafana --key tls-grafana-$EXTERNAL_IP.key --cert tls-grafana-$EXTERNAL_IP.crt -n monitoring --context $CLUSTER_CONTEXT_NAME
 
 echo "Installing Grafana using helm in cluster $CLUSTER_CONTEXT_NAME"
-helm install grafana grafana/grafana --version $grafanaHelmChartVerion --namespace  monitoring  --set persistence.enabled=true --set ingress.enabled=true --set ingress.hosts="{grafana.monitoring.$EXTERNAL_IP.nip.io}" --set ingress.tls[0].secretName=tls-grafana --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx  --kube-context $CLUSTER_CONTEXT_NAME
+helm install grafana grafana/grafana --version $grafanaHelmChartVerion --namespace  monitoring  --set persistence.enabled=true --set ingress.enabled=true --set ingress.hosts="{grafana.monitoring.$EXTERNAL_IP.nip.io}" --set ingress.tls[0].secretName=tls-grafana --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx  --kube-context $CLUSTER_CONTEXT_NAME --set datasources."datasource\.yaml\.apiVersion"=1 --set datasources."datasource\.yaml\.datasources[0].name"=Prometheus  --set datasources."datasource\.yaml\.datasources[0].type"=Prometheus  --set datasources."datasource\.yaml\.datasources[0].url"=http://prometheus-server.monitoring.svc.cluster.local --set datasources."datasource\.yaml\.datasources[0].isDefault"=true
 echo "Retrieving Grafana login password from cluster $CLUSTER_CONTEXT_NAME"
 GRAFANA_PASSWORD=`kubectl get secret  --context $CLUSTER_CONTEXT_NAME --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo`
 
