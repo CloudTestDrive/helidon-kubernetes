fe935f5280590ba7b6127b7efff65d0eb5fc9f33
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-02 14:41:13 +0100

    upgrade to latest prometheus and grafana helm chart versions

--- setup/kubernetes-labs/helmChartVersions.sh
+++ setup/kubernetes-labs/helmChartVersions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ingressHelmChartVersion=4.1.4
 dashboardHelmChartVersion=5.7.0
-prometheusHelmChartVersion=15.10.1
-grafanaHelmChartVerion=6.30.3
+prometheusHelmChartVersion=15.11.0
+grafanaHelmChartVerion=6.32.10
 elasticSearchHelmChartVersion=7.17.3
 metricsServerHelmChartVersion=3.8.2
 prometheusStackHelmChartVersion=39.2.1
