bc3e6c85d9fa6fa61afe2993ff09792df592c2cc
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-04 13:22:36 +0100

    update the outputs to reflect the new nsg map type output

--- setup/common/oke-setup/oke-terraform-config/oke-outputs.tf
+++ setup/common/oke-setup/oke-terraform-config/oke-outputs.tf
@@ -10,10 +10,12 @@ output "subnet_ids" {
   description = "map of subnet ids (worker, int_lb, pub_lb) used by OKE."
   value       = module.oke.subnet_ids
 }
+# deprecated
 output "int_lb_nsg" {
   description = "id of default NSG that can be associated with the internal load balancer"
   value       = module.oke.int_lb_nsg
 }
+# deprecated
 output "pub_lb_nsg" {
   description = "id of default NSG that can be associated with the internal load balancer"
   value       = module.oke.pub_lb_nsg
@@ -21,4 +23,8 @@ output "pub_lb_nsg" {
 output "kubeconfig" {
   description = "convenient command to set KUBECONFIG environment variable before running kubectl locally"
   value       = "export KUBECONFIG=generated/kubeconfig"
+}
+output "nsg_ids" {
+  description = "map of NSG ids (cp, worker, int_lb, pub_lb, pod) used by OKE."
+  value       = module.oke.nsg_ids
 }
