c47e214d5a2b2c493e255ed2a3a21f1e27762d3f
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-05 11:32:19 +0100

    quiet the curl

--- setup/common/oci-osok/download-operator-sdk.sh
+++ setup/common/oci-osok/download-operator-sdk.sh
@@ -77,7 +77,7 @@ export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm6
 export OS=$(uname | awk '{print tolower($0)}')
 
 export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.22.2
-curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
+curl -s -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
 chmod +x operator-sdk_${OS}_${ARCH} && mv operator-sdk_${OS}_${ARCH} $OPERATOR_SDK_PATH
 
 cd $SAVED_DIR
