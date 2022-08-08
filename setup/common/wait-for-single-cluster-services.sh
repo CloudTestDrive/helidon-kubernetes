80d6ef191eee8c51bb838b0a49dff23e21e3d4f9
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-08 14:32:46 +0100

    swithc location of waiting for cluster and make it paramed based on the cluster type not, not the cluster OCID

--- setup/common/wait-for-single-cluster-services.sh
+++ setup/common/wait-for-single-cluster-services.sh
@@ -10,18 +10,19 @@ else
   echo "Using default context name of $CLUSTER_CONTEXT_NAME"
 fi
 
-OKE_OCID_NAME=`bash ../common/setting/to-valid-name.sh OKE_OCID_$CLUSTER_CONTEXT_NAME`
+
+KUBERNETES_CLUSTER_TYPE_NAME=`bash ./settings/to-valid-name.sh "KUBERNETES_CLUSTER_TYPE_"$CLUSTER_CONTEXT_NAME`
 
 echo "Waiting for core services to be available." 
 export WAIT_LOOP_COUNT=180
 
-bash ../common/wait-for-service-availability.sh $OKE_OCID_NAME
+bash ../common/wait-for-service-availability.sh $KUBERNETES_CLUSTER_TYPE_NAME
 
 RESP=$?
 
 if [ $RESP -ne 0 ]
 then
-  echo "One of more of the services DB_OCID $OKE_OCID_NAME IMAGES_READY did not start within $WAIT_LOOP_COUNT test loops"
+  echo "One of more of the services $KUBERNETES_CLUSTER_TYPE_NAME did not start within $WAIT_LOOP_COUNT test loops"
   echo "Cannot continue"
   exit $RESP
 fi
