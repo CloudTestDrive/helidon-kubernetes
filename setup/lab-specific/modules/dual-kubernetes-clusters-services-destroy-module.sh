951f486f399110a87560ed9ce48350b1a768f1fc
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-08 17:31:58 +0100

    actually destroy things when asked to, nto try setting thme up again

--- setup/lab-specific/modules/dual-kubernetes-clusters-services-destroy-module.sh
+++ setup/lab-specific/modules/dual-kubernetes-clusters-services-destroy-module.sh
@@ -16,7 +16,7 @@ SAVED_PWD=`pwd`
 cd $KUBERNETES_OPTIONAL_LABS_DIR
 bash ./monitoring-destroy.sh $CLUSTER_CONTEXT_NAME_ONE
 bash ./monitoring-destroy.sh $CLUSTER_CONTEXT_NAME_TWO
-bash ./logs-to-ooss-fluentd-dual-cluster-setup.sh $CLUSTER_CONTEXT_NAME_ONE $CLUSTER_CONTEXT_NAME_TWO
+bash ./logs-to-ooss-fluentd-dual-cluster-destroy.sh $CLUSTER_CONTEXT_NAME_ONE $CLUSTER_CONTEXT_NAME_TWO
 
 cd $PERSISTENCE_DIR
 bash ./logger-microservice-destroy.sh "$CLUSTER_CONTEXT_NAME_ONE"
