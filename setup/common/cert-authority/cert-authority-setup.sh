d001e380270c62c44aa36350c4fc58307facf53f
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-10 17:37:16 +0100

    better error checking and spacing

--- setup/common/cert-authority/cert-authority-setup.sh
+++ setup/common/cert-authority/cert-authority-setup.sh
@@ -172,6 +172,12 @@ then
       echo "CA named $CA_NAME doesn't exist, creating it, there may be a short delay"
       echo "Creating certificate authority"
       CERT_AUTHORITY_OCID=`oci certs-mgmt certificate-authority create-root-ca-by-generating-config-details --compartment-id $COMPARTMENT_OCID --name $CA_NAME --subject "CN=LabsCA" --kms-key-id $VAULT_KEY_OCID | jq -j '.data.id'`
+      RESP=$?
+      if [ "$RESP" -ne 0 ]
+      then
+        echo "Unexpected non zero response ( $RESP) creating cert authority, unable to continue"
+        exit $RESP
+      fi
       echo "CA being created using OCID $CERT_AUTHORITY_OCID"
       echo "CERT_AUTHORITY_OCID=$CERT_AUTHORITY_OCID" >>$SETTINGS
       echo "CERT_AUTHORITY_REUSED=false" >> $SETTINGS
