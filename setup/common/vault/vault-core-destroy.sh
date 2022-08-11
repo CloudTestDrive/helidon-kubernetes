4e6e644f9bd6f21d4601307ddff754b70facbc20
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-11 09:09:47 +0100

    get the error codes for checking

--- setup/common/vault/vault-core-destroy.sh
+++ setup/common/vault/vault-core-destroy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -f
 
 bash ./vault-key-destroy.sh RSA
+RESP=$?
 if [ $RESP -ne 0 ]
 then
   echo "Vault-key-destroy on key RSA returned an error, unable to continue"
   exit $RESP
 fi
 bash ./vault-key-destroy.sh AES
-
+RESP=$?
 if [ $RESP -ne 0 ]
 then
   echo "Vault-key-destroy on key AES returned an error, unable to continue"
--- setup/common/vault/vault-core-setup.sh
+++ setup/common/vault/vault-core-setup.sh
@@ -8,14 +8,14 @@ then
   exit $RESP
 fi
 bash ./vault-key-setup.sh AES AES 32
-
+RESP=$?
 if [ $RESP -ne 0 ]
 then
   echo "Vault-key-setup on key AES returned an error, unable to continue"
   exit $RESP
 fi
 bash ./vault-key-setup.sh RSA RSA 512
-
+RESP=$?
 if [ $RESP -ne 0 ]
 then
   echo "Vault-key-setup on key RSA returned an error, unable to continue"
