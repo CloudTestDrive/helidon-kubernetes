e34bf31ee2c088521f357589ea5e8063e194bc88
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-17 21:21:35 +0100

    temp fix until OU update their lab text

---
+++ setup/kubernetes-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -f
+echo "This script is a temporary fix until the OU documentation is updated, it "
+echo "is switching to the  $HOME/helidon-kubernrtes/setup/common/oke-setup and"
+echo "Then running the oke-cluster-setup.sh script"
+
+
+cd oke-setup
+if [ $# -gt 0 ]
+then
+  bash oke-cluster-setup.sh $1
+else
+  bash oke-cluster-setup.sh
+fi
