0abe70e0359fe3790e20e80971d6a1549ec514ea
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-08-19 16:07:26 +0100

    use more stavdard names for vars

--- setup/common/update-file.sh
+++ setup/common/update-file.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -f
-updatefile=$1
-oldtext=$2
-newtext=$3
+UPDATE_FILE=$1
+OLD_TEXT=$2
+NEW_TEXT=$3
 if [ $# -gt 3 ]
 then
   SED_SEP=$4
 else
   SED_SEP=/
 fi
-echo "Updating $updatefile replacing $oldtext with $newtext using separator $SED_SEP"
-temp="$updatefile".tmp
-SED_CMD="s""$SED_SEP""$oldtext""$SED_SEP""$newtext""$SED_SEP""g"
+echo "Updating $UPDATE_FILE replacing $OLD_TEXT with $NEW_TEXT using separator $SED_SEP"
+TEMP_FILE="$UPDATE_FILE".tmp
+SED_CMD="s""$SED_SEP""$OLD_TEXT""$SED_SEP""$NEW_TEXT""$SED_SEP""g"
 #echo "Sed command is :""$SED_CMD"
-cat $updatefile | sed -e "$SED_CMD" > $temp
-rm $updatefile
-mv $temp $updatefile
+cat $UPDATE_FILE | sed -e "$SED_CMD" > $TEMP_FILE
+rm $UPDATE_FILE
+mv $TEMP_FILE $UPDATE_FILE
