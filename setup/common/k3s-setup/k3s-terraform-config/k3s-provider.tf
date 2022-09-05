6ae434dbb969b9de3790ba3149087879d8519e14
tim_graves <28924492+atimgraves@users.noreply.github.com>
2022-09-05 12:52:44 +0100

    lates k3s TF change support

--- setup/common/k3s-setup/k3s-terraform-config/k3s-provider.tf
+++ setup/common/k3s-setup/k3s-terraform-config/k3s-provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "PROVIDER_VERSION"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    oci = {
+#      source  = "oracle/oci"
+#      version = "PROVIDER_VERSION"
+#    }
+#  }
+#}
 
 #provider "oci" {
 #   auth = "InstancePrincipal"
