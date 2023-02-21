2b2d93a777dd339116c99a1655f4084fef6669d1
tim_graves <28924492+atimgraves@users.noreply.github.com>
2023-02-21 16:58:31 +0000

    revert to an older version of elastic as the new one has a different access approach which will require more work

--- setup/kubernetes-labs/helmChartVersions.sh
+++ setup/kubernetes-labs/helmChartVersions.sh
@@ -3,6 +3,6 @@ ingressHelmChartVersion=4.5.2
 dashboardHelmChartVersion=6.0.0
 prometheusHelmChartVersion=19.6.1
 grafanaHelmChartVerion=6.50.7
-elasticSearchHelmChartVersion=8.5.1
+elasticSearchHelmChartVersion=7.17.3
 metricsServerHelmChartVersion=3.8.3
 prometheusStackHelmChartVersion=45.1.1
