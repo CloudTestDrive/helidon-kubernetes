55790c4aedfc45c098765ccfa8d24388f6d97a33
tim_graves <28924492+atimgraves@users.noreply.github.com>
2023-08-22 19:27:16 +0100

    fit to vault secret already exists logic

--- setup/common/vault/vault-individual-secret-setup.sh
+++ setup/common/vault/vault-individual-secret-setup.sh
@@ -84,7 +84,7 @@ fi
 BASE64_VAULT_SECRET_VALUE=`echo $VAULT_SECRET_VALUE | base64`
 #lets see it it exists already
 echo "Checking if secret $VAULT_SECRET_NAME already exists"
-VAULT_SECRET_OCID=`oci vault secret list --compartment-id $COMPARTMENT_OCID --all --lifecycle-state ACTIVE --name $VAULT_SECRET_NAME --vault-id $VAULT_OCID | jq -j '.data[0].id'`
+VAULT_SECRET_OCID=`oci vault secret list --compartment-id $COMPARTMENT_OCID --all --lifecycle-state ENABLED --name $VAULT_SECRET_NAME --vault-id $VAULT_OCID | jq -j '.data[0].id'`
 
 VAULT_SECRET_PENDING_DELETION_OCID=`oci vault secret list --compartment-id $COMPARTMENT_OCID --all --lifecycle-state PENDING_DELETION --name $VAULT_SECRET_NAME --vault-id $VAULT_OCID | jq -j '.data[0].id'`
 if [ -z $VAULT_SECRET_PENDING_DELETION_OCID ]
