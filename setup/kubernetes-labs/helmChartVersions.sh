a5a90a4aa35ea493e50d1f1cf6be3c933ce691b5
tim_graves <28924492+atimgraves@users.noreply.github.com>
2023-09-22 14:33:37 +0100

    update to latest OKE and helm chart versions

--- setup/kubernetes-labs/helmChartVersions.sh
+++ setup/kubernetes-labs/helmChartVersions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ingressHelmChartVersion=4.6.1
-dashboardHelmChartVersion=6.0.7
-prometheusHelmChartVersion=22.4.1
-grafanaHelmChartVerion=6.56.2
+ingressHelmChartVersion=4.7.1
+dashboardHelmChartVersion=6.0.8
+prometheusHelmChartVersion=24.0.0
+grafanaHelmChartVerion=6.59.0
 elasticSearchHelmChartVersion=8.5.1
-metricsServerHelmChartVersion=3.10.0
+metricsServerHelmChartVersion=3.11.0
 prometheusStackHelmChartVersion=45.7.1
