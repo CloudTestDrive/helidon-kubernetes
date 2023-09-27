5e1fbdd88d56508dfdd517279b3f75cd28133740
tim_graves <28924492+atimgraves@users.noreply.github.com>
2023-09-27 16:15:54 +0100

    added support to set the OS version

--- setup/common/oke-setup/oke-terraform-config/oke-module.tf
+++ setup/common/oke-setup/oke-terraform-config/oke-module.tf
@@ -30,7 +30,7 @@ module oke {
   calico_mode              = "CALICO_MODE"
 
   node_pools = {
-    POOL_NAME = { shape = "WORKER_SHAPE", ocpus = WORKER_OCPUS, memory = WORKER_MEMORY, node_pool_size = WORKER_COUNT, boot_volume_size = WORKER_BOOT_SIZE, label = {pool = "POOL_NAME" } }
+    POOL_NAME = { shape = "WORKER_SHAPE", ocpus = WORKER_OCPUS, memory = WORKER_MEMORY, node_pool_size = WORKER_COUNT, boot_volume_size = WORKER_BOOT_SIZE, label = {pool = "POOL_NAME" } NODE_POOL_OS_OPTION}
   } 
 
    providers = {
