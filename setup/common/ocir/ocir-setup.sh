39158432e7e21acd72aed5889e096da67303fcf0
tim_graves <28924492+atimgraves@users.noreply.github.com>
2024-02-21 16:28:28 +0000

    better error handling of missing repos

--- setup/common/ocir/ocir-setup.sh
+++ setup/common/ocir/ocir-setup.sh
@@ -72,6 +72,10 @@ fi
 
 echo "Checking for existing OCIR repo $OCIR_REPO_NAME"
 OCIR_REPO_OCID=`oci artifacts container repository list --compartment-id $COMPARTMENT_OCID --display-name $OCIR_REPO_NAME --all | jq -j '.data.items[0].id' `
+if [ -z $OCIR_REPO_OCID ] 
+then
+	OCIR_REPO_OCID = 'null'
+fi
 if [ $OCIR_REPO_OCID = 'null' ]
 then
 # No existing repo for stock manager
