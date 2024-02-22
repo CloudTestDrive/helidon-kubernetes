eb8c3b6474d8afcb64b72894ea6e314ce185182f
tim_graves <28924492+atimgraves@users.noreply.github.com>
2024-02-22 13:12:49 +0000

    try older chart due to version issue

--- setup/kubernetes-labs/helmChartVersions.sh
+++ setup/kubernetes-labs/helmChartVersions.sh
@@ -2,7 +2,7 @@
 ingressHelmChartVersion=4.9.1
 dashboardHelmChartVersion=6.0.8
 prometheusHelmChartVersion=25.13.0
-grafanaHelmChartVerion=7.3.1
+grafanaHelmChartVerion=7.3.0
 elasticSearchHelmChartVersion=8.5.1
 metricsServerHelmChartVersion=3.12.0
 prometheusStackHelmChartVersion=56.8.2
