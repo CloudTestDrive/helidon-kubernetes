6239dfa99512fe83bb859617f524b0ee5a432abe
tim_graves <28924492+atimgraves@users.noreply.github.com>
2025-10-22 14:53:39 +0100

    update to latest helm chart versions

--- setup/kubernetes-labs/helmChartVersions.sh
+++ setup/kubernetes-labs/helmChartVersions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ingressHelmChartVersion=4.12.1
-dashboardHelmChartVersion=7.11.1
-prometheusHelmChartVersion=27.7.1
-grafanaHelmChartVerion=8.11.0
+ingressHelmChartVersion=4.12.3
+dashboardHelmChartVersion=7.13.0
+prometheusHelmChartVersion=27.20.0
+grafanaHelmChartVerion=9.2.4
 elasticSearchHelmChartVersion=8.5.1
 metricsServerHelmChartVersion=3.12.2
-prometheusStackHelmChartVersion=70.3.0
+prometheusStackHelmChartVersion=75.1.0
 
 
 # used for k8s 1.30.2
