73120ced06a72f4bf268e2e33ce0e0eb249da692
tim_graves <28924492+atimgraves@users.noreply.github.com>
2025-10-22 12:49:28 +0100

    stopped db logic fix

--- setup/common/database-setup.sh
+++ setup/common/database-setup.sh
@@ -103,10 +103,10 @@ if [ -z $DB_OCID ]
   DB_OCID_STOPPED=`oci db autonomous-database list --compartment-id $COMPARTMENT_OCID --display-name $DBNAME --lifecycle-state STOPPED | jq -j '.data[0].id'`
   if [ -z "$DB_OCID_STOPPED" ]
   then
+      echo "No stopped database named $DBNAME located, continuing"
+  else 
       echo "There is already a database with the name $DBNAME but it's in stopped state, you need to start it and then re-run this script to use it or change the name of the database so it doesn't conflict"
       exit 30
-  else 
-      echo "No stopped database named $DBNAME located, continuing"
   fi
   DB_OCID=`oci db autonomous-database list --compartment-id $COMPARTMENT_OCID --display-name $DBNAME --lifecycle-state AVAILABLE | jq -j '.data[0].id'`
   if [ -z "$DB_OCID" ]
